a517ae89c675bcee10bd188d0b112220a6c23d9a
Rakan-Almutiri <44053941+Rakan-Almutiri@users.noreply.github.com>
2018-12-02 10:22:01 +0300

    q

--- example/firstapp/models.py
+++ example/firstapp/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 
 class Car(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     price = models.FloatField()
     quantity = models.IntegerField()
